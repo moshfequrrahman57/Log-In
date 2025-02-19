20dbb148c507b130611a0fce095beba86b8cf296
Moshfequr <moshfequrrahman57@gmail.com>
2025-02-20 02:38:18 +0600

    password password state

---
+++ script.js
@@ -0,0 +1,7 @@
+const menu_btn=document.getElementById("menu_btn");
+const menu_items=document.getElementById("menu_items");
+
+menu_btn.onclick=()=>{
+
+    menu_items.classList.toggle("active");
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    extend:{
+      colors: {
+        'light-chocolate': '#fefae0',
+      },
+    }
   },
   plugins: [],
 }
