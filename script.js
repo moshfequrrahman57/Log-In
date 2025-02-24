fb8d25bd5ecb173f8835293a422ea6e74ca55d81
Moshfequr <moshfequrrahman57@gmail.com>
2025-02-24 13:35:25 +0600

    log in successfull

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ menu_btn.onclick=()=>{
 }
 
 
-const email=document.getElementById("email");
+// const email=document.getElementById("email");
 
 const email_placeholder=document.getElementById("email_placeholder");
 const email_input=document.getElementById("email_input");
@@ -50,4 +50,41 @@ input_checkbox.onclick=()=>{
     }
 }
 
-   
+
+const submit_btn=document.getElementById("submit_btn");
+submit_btn.onclick=()=>{
+    console.log(email_input.value,pass_input.value);
+    let email_id=email_input.value;
+    let pass=pass_input.value;
+    let modified_email=email_id.replace(/\s/g,""); //removes all space
+    console.log(modified_email);
+
+    if(modified_email==="abc@hello.com" && pass==="123"){
+      //  console.log(" Authentic");
+        
+        window.location='/homepage.html';
+    }
+    else{
+        console.log("Log in failed");
+    }
+}
+
+const copy_btn=document.getElementById("copy_btn");
+
+  //console.log(copy_btn); 
+
+  copy_btn.onclick=()=>{
+    email_input.value="abc@hello.com";
+    email_input.focus();
+    email_input.select();
+    navigator.clipboard.writeText("Noman");
+  }
+
+  const innerbody=document.body.innerHTML;
+
+  document.body.onload=()=>{
+    console.log("Page on load");
+    email_input.value="";
+    pass_input.value="";
+     
+  }
